e8f5891018bed303ed55d6d9eabf75f53073d558
Larins <69240139+Larins@users.noreply.github.com>
2020-11-19 10:11:14 +0100

    Update de la Fase 01 en Java

---
+++ M04_Ejercicios/src/m04_ejercicios/M04_Fase01.java
@@ -0,0 +1,32 @@
+package m04_ejercicios;
+import java.util.*;
+
+public class M04_Fase01 {
+	
+	public static void main(String[] args) {
+				
+		//Declarar e inicializar las variables de los billetes.
+		
+		int eu5 = 5;
+		int eu10 = 10;
+		int eu20 = 20;
+		int eu50 = 50;
+		int eu100 = 100;
+		int eu200 = 200;
+		int eu500 = 500;
+		
+		//Declarar la variable para el precio total de la comida.
+		
+		int ticket = 0;
+		
+		//Declarar el array con los platos del menú.
+		
+		ArrayList<String> menu = new ArrayList<>();
+		
+		//Declarar el array con los precios de los platos.
+		
+		ArrayList<Integer> precios = new ArrayList<>();
+
+	}
+
+}
